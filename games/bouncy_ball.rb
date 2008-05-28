2d8b7391ac9289dc0b05aea771f4f7b2a02f212e
John Croisant <jacius@gmail.com>
2008-05-28 02:54:27 -0500

    Added game setup and loop. All parts complete.

--- games/bouncy_ball.rb
+++ games/bouncy_ball.rb
@@ -2,97 +2,135 @@
 
 THE GAME:
 
-  A ball bounces inside a cup. Point and click the ball to
+  A ball bounces inside a room. Point and click the ball to
   gain a point and send it up into the air. But if it touches
-  the bottom of the cup, you lose a point!
+  the floor, you lose a point!
 
   Get 15 points to win!
 
 
 DEMONSTRATES:
 
-  * Visible shapes (Circle, Lines).
-  * Physical shapes which fall and bounce.
+  * GameObject class inheritance.
+  * Visible shapes (Circle, Line).
+  * Loose physical shapes which fall and bounce.
+  * Static physical shapes which don't move.
+  * The Game class.
+  * The World class.
   * The when_clicked event hook.
   * The when_collided event hook.
-
+  
 
 CURRENT STATUS:
 
-  * Incomplete (needs game loop).
-  * Does not run.
+  * All game parts complete.
+  * Library requirements not implemented.
+
+
+AUTHOR:
+
+  John Croisant, 2008-05-27
 
 =end
 
 
-PI = Math::PI
-
 
 # Represents a bouncing ball.
 class Ball < GameObject
-  
   # Set up the physical and visual properties of the ball.
   def initialize( params )
     base_params = { 
       :center  => v(0,0),
-      :radius  => 1.0,
+      :radius  => 15.0,
       :density => 5.0,
-      :elast   => 0.9, 
-      :solid   => true,
-      :emit    => true,
-      :gravity => v(0,-9.8),
+      :elast   => 0.9,
       :color   => :red
     }
     
-    @shape = Circle.new( base_params.merge(params) )
+    add_shape Circle.new( base_params.merge(params) )
   end
-
 end
 
 
-# Create walls to bounce off of.
+class Wall < GameObject
+  def initialize( p1, p2 )
+    base_params = { 
+      :color     => :white,
+      :thickness => 3.0,
+      :static    => true,
+      :elast     => 1.0
+    }    
+  
+    add_shape Line.new( base_params.merge(:points => [p1,p2]) )
+  end
+end
+
+
+
+def make_game
+  game = Game.new
+  
+  game[:score] = 0
+  game.background = :black
+  
+  game.when_key_pressed(:q) do
+    puts "Bye!"
+    exit
+  end
+  
+  game.when_true( proc{ game[:score] >= 15 } ) do
+    puts "You got 15 points! You win! \m/"
+    exit
+  end
+  
+  return game
+end
+
+
+def make_world
+  return World.new( :gravity => v(0,-9.8) )
+end
+
+
+def make_ball
+  return Ball.new
+end
+
+
 def make_walls
-  corners = [ v(-100, 400),
-              v(-100,   0),
-              v( 100,   0),
-              v( 100, 400) ]
-  
-  base_params = { 
-    :color     => :black,
-    :thickness => 3.0,
-    :solid     => true,
-    :static    => true,
-    :elast     => 1.0
-  }
-
-  corners.enum_cons(2).collect { |p1, p2|
-    Line.new( base_params.merge(:points => [p1,p2]) )
-  }
+  points = [ v( 100,   0), v( 100, 400), v(-100, 400), v(-100,   0) ]
+  # Make a wall between each pair of points.
+  return points.enum_cons(2).collect { |points| Wall.new( *points )}
 end
 
 
-def setup
-
-  $score = 0
-  
-  ball = Ball.new
-  ball.when_clicked { |click|
-    # When you click the ball:
-    #  - Gain a point.
-    #  - Push the ball away from the cursor.
-
-    $score += 1
-    @shape.push( :from => click.pos, :force => 30.0 )
-  }
-  
-  walls = make_walls
-
-  bottom = walls[1]
-  bottom.when_collided( :with => ball ) { |b|
-    # When the ball hits the bottom, lose a point,
-    # but don't fall below 0.
-
-    $score -= 1 unless $score - 1 < 0
-  }
+def make_floor
+  return Wall.new( v(-100, 5), v(100, 5) )
 end
 
+
+
+def main
+  $game = make_game
+  $game[:walls] = make_walls
+  $game[:floor] = make_floor
+  $game[:ball]  = make_ball
+  
+  $game.world = make_world
+  $game.world.add( floor, ball, walls )
+
+  $game[:ball].when_clicked do |click|
+    $game[:score] += 1
+    $game[:ball].shove( :from => click.pos, :strength => 30.0 )
+  end
+  
+  $game[:floor].when_collided( :with => $game[:ball] ) do
+    $game[:score] -= 1 unless $game[:score] - 1 < 0
+  end
+  
+  $game.after_update do
+    $game.title = "Bounce the ball! (%d pts)"%$game[:score]
+  end
+  
+  $game.go
+end
