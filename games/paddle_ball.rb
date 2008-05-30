849dd56ca806353bad0b1738717ba30d281bbf69
John Croisant <jacius@gmail.com>
2008-05-30 00:14:30 -0500

    Imported game description header.

---
+++ games/paddle_ball.rb
@@ -0,0 +1,31 @@
+=begin
+
+THE GAME:
+
+  A ball flies back and forth across a playing field. Use the arrow
+  keys to move your paddle up and down to deflect the ball. If the
+  ball flies off your opponent's side of the screen, you get a point,
+  but if it flies off your side of the screen, your opponent gets the
+  point. The first player to get 10 points wins the game!
+
+
+DEMONSTRATES:
+
+  * Keyboard input
+  * Player-controlled game objects
+  * AI-controlled game objects
+  * Collision detection
+  * Rectangle shape
+  * Non-physics-driven movement
+  
+
+CURRENT STATUS:
+
+  * Nonfuncitonal (library requirements not implemented.)
+
+
+AUTHOR:
+
+  John Croisant, 2008-05-29
+
+=end
