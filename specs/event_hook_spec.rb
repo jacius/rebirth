18f8ee71ad2986ba7be24e0702b6687d4655258a
John Croisant <jacius@gmail.com>
2008-05-31 17:02:40 -0500

    Added description of keyboard and mouse event hooks.

--- specs/event_hook_spec.rb
+++ specs/event_hook_spec.rb
@@ -15,6 +15,34 @@ describe "event hook" do
   it "should yield an event to the block"
 
 
+
+  describe "(keyboard)" do
+    
+    it "should have a :key option"
+    it "should reject non-symbol values for :key"
+    it "should have a default :key of :any"
+
+    it "should take a :modifiers option"
+    it "should reject non-Array values for :modifiers"
+    it "should have an empty Array of :modifiers by default"
+
+  end
+
+
+  describe "(mouse)" do
+    
+    it "should have a :button option"
+    it "should reject non-symbol values for :button"
+    it "should have a default :button of :any"
+
+    it "should take a :modifiers option"
+    it "should reject non-Array values for :modifiers"
+    it "should have an empty Array of :modifiers by default"
+
+  end
+
+
+
 end
 
 
