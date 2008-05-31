a347ee39940b2c311863e4311caf4a2db39fae07
John Croisant <jacius@gmail.com>
2008-05-31 16:50:29 -0500

    Added description of a generic event hook.

---
+++ specs/event_hook_spec.rb
@@ -0,0 +1,20 @@
+
+describe "event hook" do
+
+  
+  it "should take a hash of options"
+  it "should have a :name option"
+  it "should reject :name that is not a string or symbol"
+
+  it "should have a :priority option"
+  it "should reject non-numeric priorities"
+  it "should have a default priority of 0"
+  it "should run before hooks with lower priorities"
+
+  it "should take a block"
+  it "should yield an event to the block"
+
+
+end
+
+
