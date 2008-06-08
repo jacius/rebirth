c7b304c712d3759b4c2a7e6d21d6d5e96a2c14e7
John Croisant <jacius@gmail.com>
2008-06-08 18:13:54 -0500

    Added Rectangle demo.

---
+++ demos/03_rectangle_demo.rb
@@ -0,0 +1,34 @@
+#
+#
+#  DEMO: Rectangle
+#
+#  DESC: Displays a solid white rectangle. Quits when Q key pressed.
+#
+#  DATE: 2008-06-08
+#
+#
+
+require 'rebirth'
+include Rebirth
+
+view = View.open([640,480])
+
+evm = EventManager.new
+
+evm.append_hook(:trigger => KeyPressedTrigger.new(:q),
+                :action  => BlockAction.new { throw :quit } )
+
+rect = Rectangle.new( :size => [128,256] )
+
+# Draw when DrawEvent received.
+rect.append_hook(:trigger => InstanceOfTrigger.new(DrawEvent),
+                 :action  => BlockAction.new { rect.draw(view) } )
+
+# Register to receive TickEvents from the event manager.
+evm.register_listener( rect, TickEvent )
+
+catch :quit do
+  loop do
+    evm.update
+  end
+end
