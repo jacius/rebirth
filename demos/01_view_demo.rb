0138e3f214b19b48a6fd847eb7eeac7ef38727d3
John Croisant <jacius@gmail.com>
2008-06-09 20:45:07 -0500

    Added license info.

--- demos/01_view_demo.rb
+++ demos/01_view_demo.rb
@@ -7,6 +7,27 @@
 #  DATE: 2008-06-07
 #
 #
+#--
+#
+#  This file is one part of:
+#
+#  Rebirth - Game development library for Ruby
+#  Copyright (C) 2008  John Croisant
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License, version 2.1, as published by the Free Software Foundation.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# 
+#++
 
 require 'rebirth'
 include Rebirth
