1592ef863072e50423ff55dbfc094d2e2de1536a
John Croisant <jacius@gmail.com>
2009-02-14 16:16:27 -0600

    Added HasEventHandler#make_magic_hooks_for (extends Rubygame)

---
+++ lib/rebirth/event_handler.rb
@@ -0,0 +1,35 @@
+#--
+#
+#  This file is one part of:
+#
+#  Rebirth - Game development library for Ruby
+#  Copyright (C) 2008, 2009  John Croisant
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License, version 2.1, as published by the Free Software Foundation.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# 
+#++
+
+
+module Rubygame::EventHandler::HasEventHandler
+
+  # Like #make_magic_hooks, but with an owner other than self.
+  def make_magic_hooks_for( owner, hooks={} )
+    hooks.collect do |trigger, action|
+      append_hook( :owner   => owner,
+                   :trigger => _make_magic_trigger( trigger ),
+                   :action  => _make_magic_action(  action  ) )
+    end
+  end
+
+end
