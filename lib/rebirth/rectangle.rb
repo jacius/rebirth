694857c175f0688f73bc17b4b7bd4cbef9c57300
John Croisant <jacius@gmail.com>
2009-02-15 02:01:56 -0600

    Empty Rectangle class.

---
+++ lib/rebirth/rectangle.rb
@@ -0,0 +1,28 @@
+#--
+#
+#  This file is one part of:
+#
+#  Rebirth - Game development library for Ruby
+#  Copyright (C) 2008, 2009  John Croisant
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License, version 2.1, as published by the Free Software Foundation.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# 
+#++
+
+require 'rebirth/shape'
+
+
+class Rebirth::Rectangle < Rebirth::Shape
+
+end
