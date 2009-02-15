22e9b4c18020b2d7e85632fb313befe0130af4a6
John Croisant <jacius@gmail.com>
2009-02-14 19:10:17 -0600

    Added barebones Shape class.

---
+++ lib/rebirth/shape.rb
@@ -0,0 +1,36 @@
+#--
+#
+#  This file is one part of:
+#
+#  Rebirth - Game development library for Ruby
+#  Copyright (C) 2008, 2009  John Croisant
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License, version 2.1, as published by the Free Software Foundation.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# 
+#++
+
+class Rebirth::Shape
+
+  attr_reader :pos, :rot
+
+  def initialize( options={} )
+    @pos = options[:pos]
+    @rot = options[:rot]
+  end
+
+  # Draw the shape. Overridden by subclasses.
+  def draw
+  end
+
+end
