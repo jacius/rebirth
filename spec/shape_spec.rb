9af16d6103ee3164861cb826269106678fc1a7f7
John Croisant <jacius@gmail.com>
2009-02-14 19:05:20 -0600

    Made empty spec framework for Shape.

---
+++ spec/shape_spec.rb
@@ -0,0 +1,15 @@
+
+# Prefer local library over installed version.
+$:.unshift( File.join( File.dirname(__FILE__), "..", "lib" ) )
+
+require 'rebirth'
+include Rebirth
+
+
+describe Shape do
+
+  before :each do
+    @shape = Shape.new
+  end
+
+end
