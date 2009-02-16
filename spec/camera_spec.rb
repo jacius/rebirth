2b230c4d8961f9bc3c27e886eabc61011609ee68
John Croisant <jacius@gmail.com>
2009-02-16 11:40:50 -0600

    Camera should have a position.

--- spec/camera_spec.rb
+++ spec/camera_spec.rb
@@ -8,4 +8,13 @@ include Rebirth
 
 describe Camera do
 
+  before :each do
+    @camera = Camera.new
+  end
+
+  it "should have a position" do
+    @camera = Camera.new( :pos => [1,1] )
+    @camera.pos.should == [1,1]
+  end
+
 end
