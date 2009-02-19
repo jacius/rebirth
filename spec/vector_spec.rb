0484e901864b4499fcf880e5b5e9657037e5edd9
John Croisant <jacius@gmail.com>
2009-02-19 00:47:41 -0600

    Vector should store x and y as floats.

--- spec/vector_spec.rb
+++ spec/vector_spec.rb
@@ -20,4 +20,10 @@ describe Vector do
     @v.y.should == 2
   end
 
+  it "should store x and y as floats" do
+    @v = Vector.new(1,2)
+    @v.x.should be_instance_of(Float)
+    @v.x.should be_instance_of(Float)
+  end
+
 end
