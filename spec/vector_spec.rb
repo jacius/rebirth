6614347c7ee1c2038a4726af6c5caa172a9aeed1
John Croisant <jacius@gmail.com>
2009-02-19 00:42:02 -0600

    Vector should have an x component and a y component.

--- spec/vector_spec.rb
+++ spec/vector_spec.rb
@@ -7,7 +7,17 @@ include Rebirth
 
 
 describe Vector do
-  
 
+  before :each do
+    @v = Vector.new(1,2)
+  end
+  
+  it "should have an x component" do
+    @v.x.should == 1
+  end
+
+  it "should have a y component" do
+    @v.y.should == 2
+  end
 
 end
