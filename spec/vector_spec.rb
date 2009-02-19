8a3b5b8274a2a3dd209902b193dbe66bb2536a23
John Croisant <jacius@gmail.com>
2009-02-19 00:57:43 -0600

    Put vector specs so far into "(components)" category.

--- spec/vector_spec.rb
+++ spec/vector_spec.rb
@@ -8,22 +8,25 @@ include Rebirth
 
 describe Vector do
 
-  before :each do
-    @v = Vector.new(1,2)
-  end
-  
-  it "should have an x component" do
-    @v.x.should == 1
-  end
+  describe "(components)" do
+    
+    before :each do
+      @v = Vector.new(1,2)
+    end
+    
+    it "should have an x component" do
+      @v.x.should == 1
+    end
 
-  it "should have a y component" do
-    @v.y.should == 2
-  end
+    it "should have a y component" do
+      @v.y.should == 2
+    end
 
-  it "should store x and y as floats" do
-    @v = Vector.new(1,2)
-    @v.x.should be_instance_of(Float)
-    @v.x.should be_instance_of(Float)
+    it "should store x and y as floats" do
+      @v.x.should be_instance_of(Float)
+      @v.x.should be_instance_of(Float)
+    end
+
   end
 
 end
