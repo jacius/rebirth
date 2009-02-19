5990052754be363f25a1f2437308f8e98c00b3fc
John Croisant <jacius@gmail.com>
2009-02-19 00:37:46 -0600

    Empty spec framework for Vector.

---
+++ spec/vector_spec.rb
@@ -0,0 +1,13 @@
+
+# Prefer local library over installed version.
+$:.unshift( File.join( File.dirname(__FILE__), "..", "lib" ) )
+
+require 'rebirth'
+include Rebirth
+
+
+describe Vector do
+  
+
+
+end
