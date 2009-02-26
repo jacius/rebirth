aff630154ae23f6728e65353c41d8375802bc17c
John Croisant <jacius@gmail.com>
2009-02-25 23:08:11 -0600

    Shape spec uses shared drawable spec.

--- spec/shape_spec.rb
+++ spec/shape_spec.rb
@@ -5,43 +5,19 @@ $:.unshift( File.join( File.dirname(__FILE__), "..", "lib" ) )
 require 'rebirth'
 include Rebirth
 
+need { "shared_drawable_spec" }
 
 describe Shape do
 
   before :each do
     @shape = Shape.new
+    @class = Shape
   end
 
   it "should include HasTransform" do
     @shape.should be_kind_of( HasTransform )
   end
 
-  describe "(default)" do
-    
-    it "depth should be 0" do
-      @shape.depth.should == 0
-    end
-
-    it "should be visible" do
-      @shape.should be_visible
-    end
-
-  end
-
-
-  it "should have a depth" do
-    @shape = Shape.new( :depth => 1 )
-    @shape.depth.should == 1
-  end
-
-  it "should have visibility" do
-    @shape = Shape.new( :visible => false )
-    @shape.should_not be_visible
-  end
-
-
-  it "should have a draw method" do
-    @shape.should respond_to(:draw)
-  end
+  it_should_behave_like "a drawable"
 
 end
